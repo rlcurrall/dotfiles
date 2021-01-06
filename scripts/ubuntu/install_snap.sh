ff12d8ae2d4124fbc3ea5f71f7ee3c31117c92a1
Robb Currall <rlcurrall@gmail.com>
2021-01-06 10:26:49 -0500

    Add install scripts

---
+++ scripts/ubuntu/install_snap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+printf '\n\nInstalling Snap dependencies...\n\n';
+sudo snap install typora;
+sudo snap install gimp;
+sudo snap install spotify;
+sudo snap install chromium;
+sudo snap install --classic phpstorm;
