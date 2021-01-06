9f5c2694c0a568bf5fab0d21bb15d2de72b5d548
Robb Currall <rlcurrall@gmail.com>
2021-01-06 18:48:08 -0500

    Address issues from testing

--- scripts/ubuntu/install_fusuma.sh
+++ scripts/ubuntu/install_fusuma.sh
@@ -2,7 +2,6 @@
 
 # Add current user to input group
 sudo gpasswd -a "$USER" input;
-newgrp input;
 
 # Install Fusuma
 sudo gem install fusuma;
@@ -12,6 +11,9 @@ if [ -d "$HOME/.config/fusuma" ]; then
   rm -rf "$HOME/.config/fusuma";
 fi
 mkdir "$HOME/.config/fusuma";
-ln -s "$DOTFILES/fusuma" "$HOME/.config/fusuma";
+ln -s "$DOTFILES/fusuma/config.yml" "$HOME/.config/fusuma/config.yml";
 
+if [ ! -d "$HOME/.config/autostart" ]; then
+  mkdir "$HOME/.config/autostart";
+fi
 ln -s "$DOTFILES/assets/fusuma.desktop" "$HOME/.config/autostart/fusuma.desktop";
