639b4bee4fe2a2b82a7cd73ed6884db19dcce098
Robb Currall <rlcurrall@gmail.com>
2021-01-08 18:47:52 -0500

    Clean up file structure

--- scripts/general/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )";
-
-source "$DIR/install_composer.sh";
-source "$DIR/install_deno.sh";
-source "$DIR/install_nvm.sh";
-source "$DIR/install_rust.sh";
--- scripts/ubuntu/install_snap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-printf '\n\nInstalling Snap dependencies...\n\n';
-sudo snap install typora;
-sudo snap install gimp;
-sudo snap install spotify;
-sudo snap install chromium;
-sudo snap install --classic phpstorm;
